807b430725dbeabd744b9f12435e1479ba91081b
adlair <adlair.cerecedo@xamarin.com>
2016-11-02 23:04:54 -0600

    Wrongs fixed on binding, configuration script added, Storyboard added into the sample, Readme added.

---
+++ Flex/sample/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using UIKit;
+using Flex;
+
+namespace FlexSample
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register("AppDelegate")]
+	public class AppDelegate : UIResponder, IUIApplicationDelegate
+	{
+		[Export("applicationDidFinishLaunching:")]
+		public void FinishedLaunching(UIApplication application)
+		{
+#if DEBUG
+			FLEXManager sharedManager = new FLEXManager();
+			sharedManager.NetworkDebuggingEnabled = true;
+
+#endif
+		}
+
+
+	}
+}
